94c5c45c08a38187d198365eb785898bd6a8cc64
Archit Jain <jainarchit529@gmail.com>
2024-07-26 21:20:02 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Archit's Portfolio",
+  description: "A modern day resume made by modern day developer",
 };
 
 export default function RootLayout({
